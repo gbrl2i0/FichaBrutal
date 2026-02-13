56a8ffe68ca25be7949120ef3f7025025c4afb71
gbrl2i0 <gbrl2i0000@gmail.com>
2026-02-13 19:19:46 -0300

    Refactor save and load functions for ficha management

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const saveHab = document.getElementsByClassName("habilidade")
 const saveArq = document.getElementsByClassName("arquetipo")
 const saveCheck = document.getElementsByClassName("saveCheck")
 const savedText = document.getElementsByClassName("saveText")
-
+const fichaButton = document.getElementById("Fichas")
+let cacheFichas = []
 
 const Arquetypes = ["Atleta",
     "Cético",
@@ -39,46 +40,54 @@ function arquetype(arg){
 }
 
 function save(){
+    
+
     let a = ""
     for (let i = 0; i<(saveArq.length);i++ ){
-        // console.log(saveArq[i].textContent);
         a+=`${saveArq[i].textContent} `
     }
     for (let i = 0; i<(saveHab.length);i++ ){
-        // console.log(saveHab[i].checked);
         a+=`${saveHab[i].checked} `
 
     }
 
     for (let i = 0; i<(saveCheck.length);i++ ){
-        // console.log(saveCheck[i].checked);
         a+=`${saveCheck[i].checked} `
     }
 
     for (let i = 0; i<(savedText.length);i++ ){
-        // console.log(savedText[i].value);
         // a+= savedText[i].value?`${savedText[i].value} `:'vazio '
         let temp = "";
         if (savedText[i]){
         for (let j = 0; j<(savedText[i].value.length);j++ ){
             temp += savedText[i].value.charAt(j)===" "?"_":savedText[i].value.charAt(j)
-            console.log(temp)
         }}
         a+= `${temp} `
     }
+    let b =window.prompt("digite 'new' para criar uma nova ficha ou o numero da ficha à editar").toLowerCase()
     loadText.value=`${a} #`  
-    localStorage['save'] = `${a} #`; // only strings
+    if(b==="new"){localStorage['save'] += `${a} #`;
+    let newFicha = document.createElement('button')
+    newFicha.className = 'Ficha';
+    newFicha.onclick = function(){load(false,cacheFichas[newFicha.textContent])}
+    newFicha.textContent = `${document.getElementsByClassName("Ficha").length}`;
+
+    newFicha.textContent = `${document.getElementsByClassName("Ficha").length}`;
+    fichaButton.appendChild(newFicha)
+    return}
+    cacheFichas[Number(b)] = `${a} #`;
+
+
 }
 
-async function load(skip=false){
-    
+async function load(skip=false,load=loadText.value){
     let a =!skip? window.prompt("digite 'load' para confirmar").toLowerCase():"load"
     if (a === "load"){
     let temp=[];
     let t="";
-    for(let i = 0; i<loadText.value.length;i++){
-        if (loadText.value.charAt(i)!==" "){
-            t+=loadText.value.charAt(i)
+    for(let i = 0; i<load.length;i++){
+        if (load.charAt(i)!==" "){
+            t+=load.charAt(i)
         }
         else{
             temp.push(t);
@@ -89,7 +98,6 @@ async function load(skip=false){
     if(temp.length>=64){
     
     for (let i = 0; i<(saveArq.length);i++ ){
-        // console.log(saveArq[i].textContent);
         saveArq[i].textContent = temp[0];
     }
     temp.splice(0,1)
@@ -99,22 +107,44 @@ async function load(skip=false){
     temp.splice(0,saveHab.length)
 
     for (let i = 0; i<(saveCheck.length);i++ ){
-        // console.log(saveCheck[i].checked);
         saveCheck[i].checked = temp[i]==='true'?true:false
     }
     temp.splice(0,saveCheck.length)
 
     for (let i = 0; i<(savedText.length);i++ ){
-        // console.log(savedText[i].value);
         let tempText = "";
         if (savedText[i]){
         for (let j = 0; j<(temp[i].length);j++ ){
             tempText += temp[i].charAt(j)==="_"?" ":temp[i].charAt(j)
-            console.log(tempText)
         }}
         savedText[i].value=tempText
     }}
 }
         
 }
-if (localStorage['save']){loadText.value=localStorage['save'];load(true)}
+if (localStorage['save'])
+{
+    
+    let temp = ""
+    for(let i = 0; i < localStorage['save'].length;i++ ){
+        temp+=localStorage['save'].charAt(i)
+        
+        if (localStorage['save'].charAt(i)==="#"){
+            cacheFichas.push(temp)
+            let newFicha = document.createElement('button')
+            newFicha.className = 'Ficha';
+            newFicha.onclick = function(){load(false,cacheFichas[newFicha.textContent])}
+            newFicha.textContent = `${document.getElementsByClassName("Ficha").length}`;
+            temp = ""
+            fichaButton.appendChild(newFicha)
+
+        }
+
+}
+}
+    
+if (localStorage['save']){
+    loadText.value=localStorage['save'];load(true)
+
+
+}
